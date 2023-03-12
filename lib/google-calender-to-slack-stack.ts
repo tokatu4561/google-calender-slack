27eca5f4f5e959ab7670ccdb79ab11f59f96a2e8
tokatu4561 <ryobsk4561@gmail.com>
2023-03-12 11:50:04 +0900

    Initial commit

---
+++ lib/google-calender-to-slack-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class GoogleCalenderToSlackStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'GoogleCalenderToSlackQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
